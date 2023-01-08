5f773fefe6a9afb6fa94b76d6b50f5f9bf2f821f
Nikita-Baldin <nikitabaldin22102003@mail.ru>
2023-01-08 23:32:12 +0400

    ;l;

--- AISD_lab3/Source.cpp
+++ AISD_lab3/Source.cpp
@@ -2,11 +2,12 @@
 #include <iostream>
 #include <vector>
 #include <iterator>
+#include<algorithm>
 using namespace std;
 template <class T>
 class Plenty {
 private:
-	vector<T*> data;
+	vector<T> data;
 	int size;
 
 public:
@@ -21,7 +22,7 @@ public:
 
 	bool check_num(T num) {
 		for (int i = 0; i < size; i++) {
-			if (num == data.at(i)
+			if (num == data.at(i))
 				return true;
 		}
 		return false;
@@ -29,7 +30,7 @@ public:
 
 
 	T operator[](const int i) {
-		if (!data)
+		if (!data.size())
 			throw std::logic_error("arr == nullptr");
 		if (i > size - 1 || i < 0)
 			throw std::logic_error("invalide index");
@@ -39,27 +40,30 @@ public:
 	Plenty& operator+=(T num)
 	{
 		data.push_back(num);
+		size++;
 		return *this;
 	}
 	Plenty operator+(const Plenty& second) {
 		Plenty tmp;
+		tmp.size = size;
 		tmp.data = data;
 		for (int i = 0; i < second.size; i++) {
-			if (check_num(second.data.at(i) == false))
+			if (check_num(second.data.at(i)) == false)
 			{
 				tmp.data.push_back(second.data.at(i));
+				tmp.size++;
 			}
 		}
 		return tmp;
 	}
 	Plenty operator-(const Plenty& second) {
-		Plenty tmp(size);
-		tmp.data = data;
+		Plenty tmp;
+		tmp.size = size;
 		bool flag = false;
 		for (int i = 0; i < size; i++) {
 			flag = false;
 			for (int j = 0; j < second.size; j++) {
-				if (data.at(i) == second.data.at(j)
+				if (data.at(i) == second.data.at(j))
 				{
 					flag = true;
 					break;
@@ -68,28 +72,32 @@ public:
 			if (flag == false)
 			{
 				tmp.data.push_back(data.at(i));
+				tmp.size++;
 			}
 		}
 		return tmp;
 	}
 	Plenty operator+(T num) {
 		Plenty tmp;
+		tmp.size = size;
 		tmp.data = data;
 		if (check_num(num) == false) {
 			tmp.data.push_back(num);
+			tmp.size++;
 		}
 		return tmp;
 	}
 	Plenty operator-(T num)
 	{
 		Plenty tmp;
+		tmp.size = size;
 		tmp.data = data;
 		if (check_num(num) == true) {
 			for (int i = 0; i < tmp.size; i++) {
 				if (tmp.data.at(i) == num)
 				{
-					tmp.data.erase(i);
-					data_n.size--;
+					tmp.data.erase(tmp.data.begin() + i);
+					tmp.size--;
 					break;
 				}
 			}
@@ -100,9 +108,9 @@ public:
 	Plenty& operator-=(T num) {
 		if (check_num(num) == true) {
 			for (int i = 0; i < size; i++) {
-				if (data.at(i) == num)
+				if (data[i] == num)
 				{
-					data.erase(i);
+					data.erase(data.begin() + i);
 					size--;
 					break;
 				}
@@ -177,8 +185,8 @@ public:
 	}
 	friend std::ostream& operator<<(std::ostream& s, const Plenty<T>& set)
 	{
-		for (int i = 0; i < set.size; i++) {
-			s << set.data[i] << "  ";
+		for (const auto& n : set.data) {
+			s << n << " ";
 		}
 		return s;
 	}
@@ -198,19 +206,14 @@ int main() {
 		p1 += 8;
 		p1 += 3;
 		p1 += 4;
-		/*set11 += 2;
-		set11 += 3;
-		set11 += 7;
-		set11 += 8;*/
-		p11 = p1 + 5;
 		p11 = p1 - 6;
+		p11 = p11 + 5;
 		p11 -= 7;
 		std::cout << "p 1: " << p1 << std::endl;
 		std::cout << "p 11: " << p11 << std::endl;
 		std::cout << "p1+p11: " << p1 + p11 << std::endl;
 		std::cout << "p1-p11: " << p1 - p11 << std::endl;
 		std::cout << "intersection: " << p1.intersection(p11) << std::endl;
-		//std::cout << "task: " << set1.check_count(set11) << std::endl;
 		std::cout << "equality: " << (p1 == p11) << std::endl;
 		std::cout << "not equality: " << (p1 != p11) << std::endl;
 		std::cout << "index: " << p1[1] << std::endl;
@@ -222,18 +225,14 @@ int main() {
 		p2 += 8.9;
 		p2 += 1.1;
 		p2 += 4.9;
-		/*set22 += 3.3;
-		set22 += 6.4;
-		set22 += 7.7;*/
 		p22 = p2 + 4.4;
-		p22 = p2 - 6.4;
+		p22 = p22 - 6.4;
 		p22 -= 7.5;
 		std::cout << "p 2: " << p2 << std::endl;
 		std::cout << "p 22: " << p22 << std::endl;
 		std::cout << "p2+p22: " << p2 + p22 << std::endl;
 		std::cout << "p2-p22: " << p2 - p22 << std::endl;
 		std::cout << "intersection: " << p2.intersection(p22) << std::endl;
-		//std::cout << "task: " << set22.check_count(set2) << std::endl;
 		std::cout << "equality: " << (p2 == p22) << std::endl;
 		std::cout << "not equality: " << (p2 != p22) << std::endl;
 		std::cout << "index: " << p2[1] << std::endl;
@@ -245,11 +244,8 @@ int main() {
 		p3 += "kot";
 		p3 += "nen";
 		p3 += "pol";
-		/*set33 += "m";
-		set33 += "rt";
-		set33 += "w";*/
 		p33 = p3 + "ret";
-		p33 = p3 - "mom";
+		p33 = p33 - "mom";
 		p33 -= "nik";
 		p33 += "you";
 		std::cout << "p 3: " << p3 << std::endl;
@@ -257,46 +253,49 @@ int main() {
 		std::cout << "p3+p33: " << p3 + p33 << std::endl;
 		std::cout << "p3-p33: " << p3 - p33 << std::endl;
 		std::cout << "intersection: " << p3.intersection(p33) << std::endl;
-		//std::cout << "task: " << set3.check_count(set33) << std::endl;
 		std::cout << "equality: " << (p3 == p33) << std::endl;
 		std::cout << "not equality: " << (p3 != p33) << std::endl;
 		std::cout << "index: " << p3[1] << std::endl;
 		std::cout << std::endl;
-
+		
 		Plenty<std::pair<int, double>> p4, p44;
 		p4 += {1, 3.2};
 		p4 += {2, 5.7};
 		p4 += {3, 4.4};
 		p4 += {8, 9.0};
 		p4 += {10, 3.5};
-		/*set44 += {1, 3.3};
-		set44 += {1, 3.2};
-		set44 += {2, 5.6};*/
-		p44 = p4 + std::pair<int, double>(1, 4.4);
-		p44 = p4 - std::pair<int, double>(1, 3.2);
+		p44 = p4 + std::pair<int, double>{1, 4.4};
+		p44 = p44 - std::pair<int, double>{1, 3.2};
 		p44 -= {2, 5.7};
+		//operator<<(cout,p4);
+		/*
 		std::cout << "p 4: " << p4 << std::endl;
 		std::cout << "p 44: " << p44 << std::endl;
 		std::cout << "p4+p44: " << p4 + p44 << std::endl;
 		std::cout << "p4-p44: " << p4 - p44 << std::endl;
 		std::cout << "intersection: " << p4.intersection(p44) << std::endl;
-		//std::cout << "task: " << set44.check_count(set4) << std::endl;
 		std::cout << "equality: " << (p4 == p44) << std::endl;
 		std::cout << "not equality: " << (p4 != p44) << std::endl;
 		std::cout << "index: " << p4[1] << std::endl;
 		std::cout << std::endl;
+		*/
 
-
+		/*
 		Plenty<std::pair<int, double>> p, pp;
 		p += {1, 3.2};
 		p += {2, 5.7};
 		p += {3, 4.4};
-		pp = p + std::pair<int, double>(1, 4.4);
+		pp = p + std::pair<int, double>{1, 4.4};
 		p += {1, 4.4};
 		std::cout << "p: " << p << std::endl;
 		std::cout << "pp: " << pp << std::endl;
 		std::cout << "equality: " << (p == pp) << std::endl;
 		std::cout << "not equality: " << (p != pp) << std::endl;
+		
+		*/
+
+
+
 	}
 	catch (std::exception& ex)
 	{
